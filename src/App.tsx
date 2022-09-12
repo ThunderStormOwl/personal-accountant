dbc597c5ddd5cd69e77e3cb4bb6c1fca58604c7c
ThunderStormOwl <lausrock@gmail.com>
2022-09-12 13:44:44 -0300

    Context API in REACT class

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import {Dashboard} from './components/Dashboard/Dashboard'
-import {Header} from './components/Header/Index';
-import {GlobalStyle} from './styles/global';
-import {NewTransactionModal} from './components/NewTransactionModal/Index'
+import { Dashboard } from './components/Dashboard/Dashboard';
+import { Header } from './components/Header/Index';
+import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal/Index';
 import { useState } from 'react';
+import { TransactionsProvider } from './TransactionsContext';
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-    function handleOpenNewTransactionModal(){
-        setIsNewTransactionModalOpen(true);
-    }
-    function handleCloseNewTransactionModal(){
-        setIsNewTransactionModalOpen(false);
-    }
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
-    <>
-      <Header
-        onOpenNewTransactionModal={handleOpenNewTransactionModal}
-      />
-      <Dashboard/>
-      <NewTransactionModal 
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequesClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,38 +1,45 @@
-import { Container } from "./styles"
-import incomeImg from '../../assets/in.svg'
-import outcomeImg from '../../assets/out.svg'
-import totalImg from '../../assets/total.svg'
+import { useContext } from 'react';
+import { Container } from './styles';
+import incomeImg from '../../assets/in.svg';
+import outcomeImg from '../../assets/out.svg';
+import totalImg from '../../assets/total.svg';
+import { TransactionContext } from '../../TransactionsContext';
 
-export function Summary(){
-    return(
-        <Container>
-            <div>
-                <header>
-                    <p>Incoming expenses</p>
-                    <img src = {incomeImg} alt = "Income"/>
-                </header>
-                <strong>
-                    $1000,00
-                </strong>
-            </div>
-            <div>
-                <header>
-                    <p>Outgoing expenses</p>
-                    <img src = {outcomeImg} alt = "Outcome"/>
-                </header>
-                <strong>
-                    - $500,00
-                </strong>
-            </div>
-            <div className="highlight-background">
-                <header>
-                    <p>Total</p>
-                    <img src = {totalImg} alt = "Total"/>
-                </header>
-                <strong>
-                    $500,00
-                </strong>
-            </div>
-        </Container>
-    )
+export function Summary() {
+  const transactions = useContext(TransactionContext);
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Incoming expenses</p>
+          <img
+            src={incomeImg}
+            alt='Income'
+          />
+        </header>
+        <strong>$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Outgoing expenses</p>
+          <img
+            src={outcomeImg}
+            alt='Outcome'
+          />
+        </header>
+        <strong>- $500,00</strong>
+      </div>
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img
+            src={totalImg}
+            alt='Total'
+          />
+        </header>
+        <strong>$500,00</strong>
+      </div>
+    </Container>
+  );
 }
